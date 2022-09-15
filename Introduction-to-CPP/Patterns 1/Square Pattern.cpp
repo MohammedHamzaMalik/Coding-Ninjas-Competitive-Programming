02fa22f29bc7de8194a7e7729c172c560222c19e
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2022-09-15 11:08:56 +0530

    Create Square Pattern.cpp

---
+++ Introduction-to-CPP/Patterns 1/Square Pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+  	int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<n;
+        }
+        cout<<"\n";
+    }
+}
+
+
