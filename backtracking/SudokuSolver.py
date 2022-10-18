7476440d1d3683dc7d75d2b73f5926f4d4423ba4
Huzefa Taj <47708830+huzefaTaj@users.noreply.github.com>
2022-10-18 22:33:42 +0530

    Update SudokuSolver.py

--- backtracking/SudokuSolver.py
+++ backtracking/SudokuSolver.py
@@ -1,3 +1,22 @@
+"""
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+1.Each of the digits 1-9 must occur exactly once in each row.
+2.Each of the digits 1-9 must occur exactly once in each column.
+3.Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells.
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit or '.'.
+It is guaranteed that the input board has only one solution.
+
+
+"""
 board= [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]] 
 def isvalid(board,row,col,c): 
     for i in range(9):
